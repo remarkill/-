673b9a2605303966ae514cfd55fb9ca50ad03126
remarkill <155295633+remarkill@users.noreply.github.com>
2025-04-11 19:08:25 +0800

    Delete test.js

--- test.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.body.innerHTML += '<div style="padding: 20px; background: pink; color: purple; font-size: 24px;">测试页面已加载</div>';
-});
